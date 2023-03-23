966d60da7d9f53cc094a69201f65944b861351f3
Drew Cuthbertson <syeoryn@gmail.com>
2023-03-23 15:37:57 -0700

    refactor

--- initiative.py
+++ initiative.py
@@ -21,10 +21,11 @@ Done:
 + health percent can't be greater than 100% (enforce max health)
 + config file (dark theme)
 + tab header
++ reorg/ clean code
+
 
 To do:
 - PC data
-- reorg/ clean code
 - handle null/ empty inputs
 - move character data validations to dataclass (min/ max hp)
 - Allow updates to a character by adding them again
@@ -51,6 +52,21 @@ Questions:
 * what happens when form fields are empty and it's submitted?
 """
 
+ss = st.session_state
+
+def build():
+    set_styles()
+    initialize()
+
+    new_character_form()
+    damage_form()
+
+    characters_list = list(ss.characters.values())
+    characters_list.sort(key=lambda char: char.initiative, reverse=True)
+    render_initiative_board(characters_list)
+    render_character_cards(characters_list)
+
+
 # TODO: add minimum values here, instead of when writing data
 @dataclass
 class Character:
@@ -68,6 +84,7 @@ def load_characters():
         'Aby the Aboleth': Character('Aby the Aboleth', 24, 15, 16, 135)
     }
 
+
 def initialize():
     if 'characters' not in st.session_state:
         st.session_state ['characters'] = load_characters()
@@ -77,6 +94,7 @@ def initialize():
 
     if 'round' not in st.session_state:
         st.session_state['round'] = 1
+
 
 def set_styles():
     st.set_page_config(page_title="Initiative Tracker", page_icon="🐉")
@@ -90,6 +108,7 @@ def set_styles():
         unsafe_allow_html=True,
     )
 
+
 def render_char_card(character, parent=st):
     parent.subheader(character.name)
     health_percent = character.hp / character.max_hp
@@ -101,6 +120,8 @@ def render_char_card(character, parent=st):
 
 
 def render_initiative_board(characters_list, parent=st.sidebar):
+    if len(characters_list) == 0:
+        return parent.caption("Add characters to view turn order.")
     parent.caption("Round {0} - turn {1}".format(ss.round, ss.turn))
     parent.title('Current Turn: {0}'.format(characters_list[ss.turn - 1].name))
     parent.caption('On Deck: {0}'.format(characters_list[ss.turn % len(characters_list)].name))
@@ -113,7 +134,7 @@ def render_initiative_board(characters_list, parent=st.sidebar):
                                   else '' for i in x], axis=1)
     )
     if parent.button('Next Turn'):
-        if ss.turn == len(characters):
+        if ss.turn == len(ss.characters):
             ss.round += 1
             ss.turn = 1
         else:
@@ -122,14 +143,15 @@ def render_initiative_board(characters_list, parent=st.sidebar):
     parent.title('')
     parent.title('')
     with parent.form("remove", clear_on_submit=True):
-        name = st.selectbox("Character", ['--'] + list(characters.keys()))
+        name = st.selectbox("Character", ['--'] + list(ss.characters.keys()))
         if st.form_submit_button("Remove Character"):
             if name != '--':
-                del characters[name]
+                del ss.characters[name]
                 st.experimental_rerun() # forces a rerun, so other components have the right characters
 
 
 def render_character_cards(characters_list):
+    if len(characters_list) == 0: return
     cols = st.columns(2)
     i = 0
     for character in characters_list:
@@ -139,49 +161,40 @@ def render_character_cards(characters_list):
         i += 1
 
 
-set_styles()
-initialize()
-
-ss = st.session_state
-characters = ss.characters
-
-with st.form("new_character", clear_on_submit=True):
-    name_col, init_col, hp_col, ac_col, submit_col = st.columns(5)
-    name_col.text_input("Character", key="name")
-    init_col.number_input("Initiative", key="initiative", min_value=0)
-    hp_col.number_input("HP", key="hp", min_value=0, step=1)
-    ac_col.number_input("AC", key="ac", min_value=0, step=1)
-    if submit_col.form_submit_button("Add"):
-        characters[ss.name] = Character(
-                name=ss.name,
-                initiative=ss.initiative,
-                ac=ss.ac,
-                hp=ss.hp,
-                max_hp=ss.hp
-            )
+def new_character_form():
+    with st.form("new_character", clear_on_submit=True):
+        name_col, init_col, hp_col, ac_col, submit_col = st.columns(5)
+        name = name_col.text_input("Character", key="name")
+        init = init_col.number_input("Initiative", key="initiative", min_value=0)
+        hp = hp_col.number_input("HP", key="hp", min_value=0, step=1)
+        ac = ac_col.number_input("AC", key="ac", min_value=0, step=1)
+        if submit_col.form_submit_button("Add"):
+            ss.characters[name] = Character(
+                    name=name,
+                    initiative=init,
+                    ac=ac,
+                    hp=hp,
+                    max_hp=hp
+                )
 
 
-with st.form("damage", clear_on_submit=True):
-    char_col, amount_col, type_col = st.columns([2, 1, 1])
-    amount = amount_col.number_input('', step=1, min_value=0)
-    type = type_col.radio('', ['Damage', 'Heal', 'Total'])
-    name = char_col.selectbox('', ['--'] + list(characters.keys()))
-    submitted = st.form_submit_button("Ka-pow!")
-    if submitted:
-        if name != '--':
-            char = characters[name]
-            # TODO: setting logic in dataclass.  Also min/max when setting Total
-            if type == 'Total':
-                char.hp = amount
-            elif type == 'Damage':
-                char.hp = max(char.hp - amount, 0)
-            elif type == 'Heal':
-                char.hp = min(char.hp + amount, char.max_hp)
+def damage_form():
+    with st.form("damage", clear_on_submit=True):
+        char_col, amount_col, type_col = st.columns([2, 1, 1])
+        amount = amount_col.number_input('', step=1, min_value=0)
+        type = type_col.radio('', ['Damage', 'Heal', 'Total'])
+        name = char_col.selectbox('', ['--'] + list(ss.characters.keys()))
+        submitted = st.form_submit_button("Ka-pow!")
+        if submitted:
+            if name != '--':
+                char = ss.characters[name]
+                # TODO: setting logic in dataclass.  Also min/max when setting Total
+                if type == 'Total':
+                    char.hp = amount
+                elif type == 'Damage':
+                    char.hp = max(char.hp - amount, 0)
+                elif type == 'Heal':
+                    char.hp = min(char.hp + amount, char.max_hp)
 
-if len(characters) != 0:
-    characters_list = list(characters.values())
-    characters_list.sort(key=lambda char: char.initiative, reverse=True)
-    render_initiative_board(characters_list)
-    render_character_cards(characters_list)
-else:
-    st.sidebar.caption("Add characters to view turn order.")
+
+build()
